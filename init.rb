916425a1023e29c86d7ce1dd7faf088979fc4b8f
Gregory Romé <gregory.rome@gmail.com>
2010-10-24 12:02:33 +0200

    Project initialization
    Signed-off-by: Gregory Romé <gregory.rome@gmail.com>

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_scmacros do
+  name 'Redmine Scmacros plugin'
+  author 'Gregory Romé'
+  description 'Add macros related to SCM'
+  version '0.0.1'
+  url 'http://github.com/gpr/redmine_scmacros'
+  author_url 'http://github.com/gpr'
+end
